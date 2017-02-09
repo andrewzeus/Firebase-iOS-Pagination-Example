7c708ad17cbf13a974da12282542015aae23ff55
Burak Gündüz <burakgunduz@outlook.com.tr>
2017-02-09 13:39:23 +0300

    first commit

--- Firebase-iOS-Pagination/PaginationVC.swift
+++ Firebase-iOS-Pagination/PaginationVC.swift
@@ -7,29 +7,207 @@
 //
 
 import UIKit
+import Firebase
 
-class PaginationVC: UIViewController {
-
+class PaginationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var pageOnTimestamp: String!
+    var posts = [Post]()
+    
+    var postCounter = 0
+    var loadingStatus = false
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(FeedVC.reloadFeed), name: NSNotification.Name(rawValue: "reloadFeed"), object: nil)
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        self.initFirstLoading { (granted) in
+            
+            if granted == true {
+                
+                self.loadingStatus = false
+                self.tableView.reloadData()
+                
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as! FeedCell
+        cell.configure(postIn: self.posts[indexPath.row])
+        
+        return cell
+    }
+    
+    func initFirstLoading(_ completion: @escaping (Bool) -> ()) {
+        
+        self.loadingStatus = true
+        
+        let num = "-\(Timestamp)"
+        let pageOn: Double = (num as NSString).doubleValue
+        
+        let query = DataService.ds.REF_POSTS.queryOrdered(byChild: "reverseTimestamp")
+            .queryStarting(atValue: pageOn, childKey: "reverseTimestamp")
+        
+        query.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            self.postCounter = Int(snapshot.childrenCount)
+            
+            if snapshot.value is NSNull {
+                
+                print("There is no post.")
+                
+                if let alert = showErrorAlert("There is no post.", msg: "Try again later or add one.") {
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            else {
+                
+                query.queryLimited(toFirst: 5).observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    self.posts.removeAll(keepingCapacity: true)
+                    
+                    for (i, snap) in snapshot.children.enumerated() {
+                        
+                        if let postAllDict = snapshot.value as? [String: AnyObject] {
+                            if let postDict = postAllDict[(snap as AnyObject).key as String] as? [String: AnyObject] {
+                                
+                                print("Displaying \(i) post")
+                                print(postDict)
+                                
+                                let post = Post(key: (snap as AnyObject).key as String, postDict: postDict)
+                                self.posts.append(post)
+                            }
+                        }
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                        completion(true)
+                    })
+                    
+                })
+            }
+        })
+    }
+    
+    func reloadFeed() {
+        
+        print("Feed will be refreshed here later.")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        print("index cell \(indexPath.row)")
+        
+        if self.posts.count == indexPath.row + 1 && !self.loadingStatus && self.posts.count < self.postCounter {
+            
+            self.loadingStatus = true
+            
+            print("\(self.posts.count) \(self.postCounter)  ***")
+            
+            print("called load")
+            
+            self.loadMoreCompletion({ (granted) in
+                
+                if granted == true {
+                    
+                    
+                    Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (Timer) in
+                        
+                        tableView.reloadData()
+                        self.loadingStatus = false
+                        
+                    })
+                    
+                }
+            })
+        }
+    }
+    
+    func loadMoreCompletion(_ completion: @escaping (Bool) -> ()) {
+        
+        self.loadingStatus = true
+        
+        if self.posts.count < self.postCounter {
+            
+            if let lastTimestamp = self.posts.last?.reverseTimestamp {
+                
+                print("Last post's timestamp handled here: \(lastTimestamp)")
+                
+                let arr = "\(lastTimestamp)".components(separatedBy: ".")
+                let arr2 = Int(arr[1])!
+                let last = "\(Int(arr[0])! + 1).\(arr2)"
+                let checkLast = (last as NSString).doubleValue
+                
+                let query = DataService.ds.REF_POSTS.queryOrdered(byChild: "reverseTimestamp")
+                                                    .queryStarting(atValue: checkLast, childKey: "reverseTimestamp")
+                
+                query.queryLimited(toFirst: 5).observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    for (i, snap) in snapshot.children.enumerated() {
+                        
+                        print("in for loop")
+                        
+                        if let postAllDict = snapshot.value as? [String: AnyObject] {
+                            if let postDict = postAllDict[(snap as AnyObject).key as String] as? [String: AnyObject] {
+                                
+                                print("Displaying from load more \(i) post")
+                                print(postDict)
+                                
+                                let post = Post(key: (snap as AnyObject).key as String, postDict: postDict)
+                                self.posts.append(post)
+                            }
+                            
+                        }
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                        completion(true)
+                    })
+                })
+            }
+        }
+        
+    }
+    
+    @IBAction func addBtn_clicked(_ sender: Any) {
+        
+        let addTVC = self.storyboard?.instantiateViewController(withIdentifier: "AddTVC") as! AddTVC
+        self.navigationController?.pushViewController(addTVC, animated: true)
+    }
+    
 }
--- Firebase-iOS-Pagination/Post.swift
+++ Firebase-iOS-Pagination/Post.swift
@@ -7,3 +7,111 @@
 //
 
 import Foundation
+import Firebase
+
+class Post {
+    
+    fileprivate var _postKey: String?
+    fileprivate var _title: String?
+    fileprivate var _content: String?
+    
+    fileprivate var _imageName: String?
+    fileprivate var _imageUrl: String?
+    fileprivate var _ownerID: String?
+    fileprivate var _reverseTimestamp: Double?
+    fileprivate var _timestamp: String?
+    fileprivate var _timeUTC: String?
+    
+    fileprivate var _userClass: User?
+    
+    var postKey: String? {
+        return _postKey
+    }
+    
+    var title: String? {
+        return _title
+    }
+    
+    var content: String? {
+        return _content
+    }
+    
+    var imageName: String? {
+        return _imageName
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var ownerID: String? {
+        return _ownerID
+    }
+    
+    var reverseTimestamp: Double? {
+        return _reverseTimestamp
+    }
+    
+    var timestamp: String? {
+        return _timestamp
+    }
+    
+    var timeUTC: String? {
+        return _timeUTC
+    }
+    
+    var userClass: User? {
+        return _userClass
+    }
+    
+    init(key: String, postDict: [String: AnyObject]) {
+        
+        self._postKey = key
+        
+        if let title = postDict["title"] as? String {
+            self._title = title
+        }
+        
+        if let content = postDict["content"] as? String {
+            self._content = content
+        }
+        
+        if let imageName = postDict["imageName"] as? String {
+            self._imageName = imageName
+        }
+        
+        if let imageUrl = postDict["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let ownerID = postDict["ownerID"] as? String {
+            self._ownerID = ownerID
+            
+            // Implement your own referance service path.
+            DataService.ds.REF_USERS.child(ownerID).observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                if let userDict = snapshot.value as? [String: AnyObject] {
+                    
+                    
+                    self._userClass = User(userKey: snapshot.key as String, userDict: userDict)
+                }
+            })
+        }
+        
+        if let reverseTimestamp = postDict["reverseTimestamp"] as? Double {
+            
+            
+            self._reverseTimestamp = reverseTimestamp
+        }
+        
+        if let timestamp = postDict["timestamp"] as? String {
+            self._timestamp = timestamp
+        }
+        
+        if let timeUTC = postDict["timeUTC"] as? String {
+            self._timeUTC = timeUTC
+        }
+        
+    }
+    
+}
