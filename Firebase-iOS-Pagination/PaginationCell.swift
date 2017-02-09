ed88deabcd27aad415e48b6f2c6c7c83805e6c31
Burak Gündüz <burakgunduz@outlook.com.tr>
2017-02-09 13:37:22 +0300

    First Init

---
+++ Firebase-iOS-Pagination/PaginationCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaginationCell.swift
+//  Firebase-iOS-Pagination
+//
+//  Created by BURAK GÜNDÜZ on 09/02/2017.
+//  Copyright © 2017 Burak Gunduz. All rights reserved.
+//
+
+import UIKit
+
+class PaginationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Firebase-iOS-Pagination/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Firebase-iOS-Pagination
+//
+//  Created by BURAK GÜNDÜZ on 09/02/2017.
+//  Copyright © 2017 Burak Gunduz. All rights reserved.
+//
+
+import Foundation
